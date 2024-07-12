bb3394ef9b0428211a1e6d7a3d293a5e3bc596fe
HoangIt1712 <166422615+HoangIt1712@users.noreply.github.com>
2024-07-12 11:39:57 +0700

    Thuc hanh

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/navbar';
+import Video from './component/video';
+import Footer from './component/footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Video />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
